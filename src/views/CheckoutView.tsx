ae5dfbc3a1f111b249ef84ca27f52a8473ad362b
Virenza <harvey4k38@gmail.com>
2026-04-02 20:09:25 +0400

    Add mystery discount hint on checkout page

--- src/views/CheckoutView.tsx
+++ src/views/CheckoutView.tsx
@@ -631,6 +631,7 @@ function CheckoutFormWithDiscount({
               </div>
             ) : (
               <>
+                <p className="text-xs text-brand-gray-dark mb-3">No code? <span className="font-bold text-brand-black">Enter your email on our site to unlock a mystery discount of up to 10% off.</span></p>
                 <div className="flex gap-3">
                   <input
                     type="text"
